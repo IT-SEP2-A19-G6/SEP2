4f6ef52bc309e648ce8d495509bbc86687b0dbb0
273962 <48473715+273962@users.noreply.github.com>
2019-11-20 14:41:37 +0100

    datafactory implemented in serverfactory

--- src/server/model/ServerModelFactory.java
+++ src/server/model/ServerModelFactory.java
@@ -1,4 +1,11 @@
 package server.model;
 
+import server.persistence.DataFactory;
+
 public class ServerModelFactory {
+    DataFactory dataFactory;
+
+    public ServerModelFactory(DataFactory dataFactory) {
+        this.dataFactory = dataFactory;
+    }
 }
