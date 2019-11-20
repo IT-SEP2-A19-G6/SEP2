8ccad4cd2a1318887edae822384148074b719144
David Nguyen <phuc.dav@gmail.com>
2019-11-20 14:55:16 +0100

    changes to Client

--- src/client/viewModel/ViewModelFactory.java
+++ src/client/viewModel/ViewModelFactory.java
@@ -1,4 +1,26 @@
 package client.viewModel;
 
+import client.model.ModelFactory;
+import client.view.ViewHandler;
+import client.viewModel.login.LoginViewModel;
+
 public class ViewModelFactory {
+    private ModelFactory modelFactory;
+    private LoginViewModel loginViewModel;
+    private ViewHandler viewHandler;
+
+    public ViewModelFactory(ModelFactory mf) {
+        this.modelFactory = mf;
+    }
+
+    public void instantiateViewModel(ViewHandler viewHandler) {
+        this.viewHandler = viewHandler;
+        loginViewModel = new LoginViewModel(modelFactory.getLoginModel(), viewHandler);
+    }
+    public LoginViewModel getLoginViewModel() {
+        if(loginViewModel == null) {
+            loginViewModel = new LoginViewModel(modelFactory.getLoginModel(), viewHandler);
+        }
+        return loginViewModel;
+    }
 }
