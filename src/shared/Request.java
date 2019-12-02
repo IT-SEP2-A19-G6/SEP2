ff9177c00b65dd25b57b72a9dac2defc778df300
David Nguyen <phuc.dav@gmail.com>
2019-12-02 15:33:36 +0100

    Adding request enums for ticket

--- src/shared/Request.java
+++ src/shared/Request.java
@@ -11,7 +11,9 @@ public class Request implements Serializable {
     public enum TYPE {
         LOGIN_REQ, //object equals User
         LOGIN_RESPONSE, //object equals Response
-        CLOSE_CONNECTION //to close socket connection
+        CLOSE_CONNECTION, //to close socket connection
+        TICKET_SEND,
+        TICKET_RECEIVE
     }
 
     public Request(TYPE type, Object object) {
