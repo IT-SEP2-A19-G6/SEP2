9e70fe1ec0658d14ba3263eca2d532e8034aeb50
David Nguyen <phuc.dav@gmail.com>
2019-12-04 09:22:12 +0100

    serverSocket

--- src/server/model/ticket/CreateTicketServerModelHandler.java
+++ src/server/model/ticket/CreateTicketServerModelHandler.java
@@ -1,16 +1,28 @@
 package server.model.ticket;
 
+import server.exceptions.DataConnectionException;
+import server.exceptions.IncorrectCredentialsException;
+import server.persistence.login.ILoginDAO;
 import shared.Response;
 import shared.Ticket;
+import shared.clients.User;
 
 public class CreateTicketServerModelHandler implements ICreateTicketServerModel {
     //private Database database;
+    // private ILoginDAO loginDAO;
     public CreateTicketServerModelHandler(/*Database database*/) {
         // this.database = database
     }
-
     @Override
-    public Ticket sendTicket() {
-        return null;
+    public Response sendTicket(Ticket ticket) {
+
+            String createTicketMessage;
+            try {
+                createTicketMessage = loginDAO.createTicket(ticket);//make method in loginDAO in JDBC
+            } catch (IncorrectCredentialsException | DataConnectionException e) {
+                createTicketMessage = e.getMessage();
+            }
+            return new Response(ticket.getUser(), createTicketMessage);//create method to get username in Ticket
+        }
     }
 }
