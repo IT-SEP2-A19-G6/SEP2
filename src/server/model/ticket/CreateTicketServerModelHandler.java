db21e6634212915307f31067a5fb155d26e639a2
David Nguyen <phuc.dav@gmail.com>
2019-12-04 01:05:12 +0100

    ServerModel

--- src/server/model/ticket/CreateTicketServerModelHandler.java
+++ src/server/model/ticket/CreateTicketServerModelHandler.java
@@ -4,10 +4,13 @@ import shared.Response;
 import shared.Ticket;
 
 public class CreateTicketServerModelHandler implements ICreateTicketServerModel {
-
+    //private Database database;
+    public CreateTicketServerModelHandler(/*Database database*/) {
+        // this.database = database
+    }
 
     @Override
     public Ticket sendTicket() {
-
+        return null;
     }
 }
