4b2f82279de72902981010d9982144b95bdbbf7c
273962 <48473715+273962@users.noreply.github.com>
2019-12-12 08:19:41 +0100

    propertyChange

--- src/client/model/communication/TicketReplyModelHandler.java
+++ src/client/model/communication/TicketReplyModelHandler.java
@@ -1,10 +1,15 @@
 package client.model.communication;
 
 import client.network.communication.ITicketReplyClient;
+import shared.IPropertyChangeSubject;
 import shared.TicketReply;
 
-public class TicketReplyModelHandler implements ITicketReplyModel {
-    ITicketReplyClient ticketReplyClient;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class TicketReplyModelHandler implements ITicketReplyModel, IPropertyChangeSubject {
+    private PropertyChangeSupport support = new PropertyChangeSupport(this);
+    private ITicketReplyClient ticketReplyClient;
 
 
     public TicketReplyModelHandler(ITicketReplyClient ticketReplyClient) {
@@ -19,5 +24,33 @@ public class TicketReplyModelHandler implements ITicketReplyModel {
     @Override
     public void addReply(TicketReply reply) {
 
+    }
+
+    @Override
+    public void addPropertyChangeListener(String name, PropertyChangeListener listener) {
+        if (name == null){
+            support.addPropertyChangeListener(listener);
+        } else {
+            support.addPropertyChangeListener(name, listener);
+        }
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    @Override
+    public void removePropertyChangeListener(String name, PropertyChangeListener listener) {
+        if (name == null){
+            support.removePropertyChangeListener(listener);
+        } else {
+            support.removePropertyChangeListener(name, listener);
+        }
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        support.removePropertyChangeListener(listener);
     }
 }
