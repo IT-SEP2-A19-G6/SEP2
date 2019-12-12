9e6f78fe4818400cfbd97320ddd9d84d3d514aa8
273962 <48473715+273962@users.noreply.github.com>
2019-12-12 19:36:21 +0100

    Network + Model

--- src/client/network/communication/ITicketReplyClient.java
+++ src/client/network/communication/ITicketReplyClient.java
@@ -1,10 +1,11 @@
 package client.network.communication;
 
+import shared.IPropertyChangeSubject;
 import shared.TicketReply;
 
 import java.util.ArrayList;
 
-public interface ITicketReplyClient {
+public interface ITicketReplyClient extends IPropertyChangeSubject {
     void getReplies(int ticketId);
     void addReply(TicketReply reply);
 }
--- src/client/network/communication/TicketReplyClient.java
+++ src/client/network/communication/TicketReplyClient.java
@@ -2,27 +2,39 @@ package client.network.communication;
 
 import client.network.socket.IClientSocketHandler;
 import shared.IPropertyChangeSubject;
+import shared.Request;
 import shared.TicketReply;
-
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
 
-public class TicketReplyClient implements ITicketReplyClient, IPropertyChangeSubject {
-    private IClientSocketHandler clientSocketHandler;
+public class TicketReplyClient implements ITicketReplyClient {
     private PropertyChangeSupport support = new PropertyChangeSupport(this);
+    private IClientSocketHandler clientSocketHandler;
 
     public TicketReplyClient(IClientSocketHandler clientSocketHandler) {
         this.clientSocketHandler = clientSocketHandler;
+        addListeners();
+    }
+
+    private void addListeners() {
+        clientSocketHandler.addPropertyChangeListener(Request.TYPE.TICKET_REPLY_RESPONSE.name(), this::handleReplies);
+    }
+
+    private void handleReplies(PropertyChangeEvent propertyChangeEvent) {
+        ArrayList<TicketReply> replies = (ArrayList<TicketReply>) propertyChangeEvent.getNewValue();
+        support.firePropertyChange(Request.TYPE.TICKET_REPLY_RESPONSE.name(), "", replies);
     }
 
     @Override
     public void getReplies(int ticketId) {
-        
+        clientSocketHandler.sendToServer(new Request(Request.TYPE.TICKET_REPLIES_REQ, ticketId));
     }
 
     @Override
     public void addReply(TicketReply reply) {
-
+        clientSocketHandler.sendToServer(new Request(Request.TYPE.ADD_TICKET_REPLY, reply));
     }
 
     @Override
